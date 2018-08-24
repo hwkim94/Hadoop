9619c1cf01ae7353fb5494aff629f3f874071e63
hyunwoo <hwkim941213@gmail.com>
2018-08-25 04:12:53 +0900

    hadoop-init

--- HadoopStudy/src/main/java/com/ybigta/jiyoung/example1/DelayCounters.java
+++
@@ -1,6 +0,0 @@
-package com.ybigta.jiyoung.example1;
-
-public enum DelayCounters {
-	  not_available_arrival, scheduled_arrival, early_arrival, not_available_departure,
-	  scheduled_departure, early_departure;
-}
